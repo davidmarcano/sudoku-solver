350da524171d44854ae0df4d7aab6c5e25434749
CainEnabel <CaneEnabled@gmail.com>
2014-07-22 10:32:35 +0800

    Adding Parser and Puzzle Classes

---
+++ Parser.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Parser{
+	private:
+	int Array[9] = {0};
+	
+	public:
+	void ParseInput(string Input){
+		//Check for validity via a helper function
+		//Change the string values to integer values
+		//Put values into array
+	}
+}
